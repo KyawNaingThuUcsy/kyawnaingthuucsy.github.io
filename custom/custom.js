4736b6c734f1dd80741ae4c770db685a0d9c3269
Kyaw Naing Thu <kyawnaingthu225@gmail.com>
2020-08-15 12:44:48 +0630

    Added effect

--- custom/custom.js
+++ custom/custom.js
@@ -1,3 +1,18 @@
 $( document ).ready(function() {
-  
+    $('#type-content').hide();
+
+    var i = 0;
+    var txt = 'Kyaw Naing Thu'; /* The text */
+    var speed = 200; /* The speed/duration of the effect in milliseconds */
+    typeWriter();    
+    function typeWriter() {
+      if (i < txt.length) {
+        $("#type-name").append(txt.charAt(i));
+        i++;
+        setTimeout(typeWriter, speed);
+      }else{
+        $('#type-content').fadeIn('slow');
+      }
+    }
+
 });
